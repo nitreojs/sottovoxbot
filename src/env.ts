8452732cef310737f3da8951f96dd8469ee90aca
nitreojs <hello@starkow.dev>
2024-07-30 19:52:10 +0300

    chore: add REDIS_URL to .env

--- src/env.ts
+++ src/env.ts
@@ -3,4 +3,5 @@ import env from 'env-var'
 
 export class Env {
   static TOKEN = env.get('TELEGRAM_BOT_TOKEN').required().asString()
+  static REDIS_URL = env.get('REDIS_URL').required().asString()
 }
--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -1,3 +1,4 @@
+import { Env } from '@/env'
 import { Redis } from 'ioredis'
 
-export const redis = new Redis()
+export const redis = new Redis(Env.REDIS_URL)
