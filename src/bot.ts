5fe27bc3dfd002dfa9e13df464ec0dfe2d1ffa1b
nitreojs <hello@starkow.dev>
2026-06-07 15:00:29 +0300

    feat: format with @puregram/markup html instead of parse_mode

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
+import { html, markup } from '@puregram/markup'
 import { Color, Logger } from '@starkow/logger'
-import { stripIndent } from 'common-tags'
 import { randomBytes } from 'crypto'
 import { InlineKeyboard, InlineQueryResult, InputMessageContent, Telegram } from 'puregram'
 
@@ -7,23 +7,18 @@ import { Env } from './env.js'
 import { redis } from './shared/index.js'
 import { RedisMessage } from './types.js'
 
-const telegram = Telegram.fromToken(Env.TOKEN)
+const telegram = Telegram.fromToken(Env.TOKEN).extend(markup())
 
 telegram.onMessage((update) => {
   if (update.chat.type !== 'private') {
     return
   }
 
-  return update.send(
-    stripIndent`
-      hi! i'm @sottovoxbot. with me you can send a private message in the <b>chat</b> (also called "whispering") to a certain user and no one except them will be able to read it.
+  return update.send(html`hi! i'm @sottovoxbot. with me you can send a private message in the <b>chat</b> (also called "whispering") to a certain user and no one except them will be able to read it.
 
-      <b>how?</b> in the group, type @sottovoxbot message @username (or user id) and click on the button that appears.
+<b>how?</b> in the group, type @sottovoxbot message @username (or user id) and click on the button that appears.
 
-      <b>example</b>: <code>@sottovoxbot hello! how are you doing? @starkow</code> or <code>@sottovoxbot hello! 398859857</code>
-    `,
-    { parse_mode: 'html' }
-  )
+<b>example</b>: <code>@sottovoxbot hello! how are you doing? @starkow</code> or <code>@sottovoxbot hello! 398859857</code>`)
 })
 
 telegram.onCallbackQuery(async (update) => {
@@ -74,13 +69,8 @@ telegram.onInlineQuery(async (update) => {
         InlineQueryResult.article({
           id: randomBytes(16).toString('hex'),
           title: 'whisper',
-          description: stripIndent`
-            message format should be like this:
-            @sottovoxbot message @username (or user id)
-          `,
-          content: InputMessageContent.text('message format should be like this: <code>@sottovoxbot message @username</code> or <code>@sottovoxbot message 123456789</code>', {
-            parseMode: 'html'
-          })
+          description: 'message format should be like this:\n@sottovoxbot message @username (or user id)',
+          content: InputMessageContent.text(html`message format should be like this: <code>@sottovoxbot message @username</code> or <code>@sottovoxbot message 123456789</code>`)
         })
       ],
       button,
@@ -93,7 +83,9 @@ telegram.onInlineQuery(async (update) => {
 
   const recipientUserId = userId ? Number(userId) : undefined
   const recipientLabel = username ? `@${username}` : `user ${recipientUserId}`
-  const recipientMention = username ? `@${username}` : `<a href="tg://user?id=${recipientUserId}">user</a> id=${recipientUserId}`
+  const recipientMention = username
+    ? `@${username}`
+    : html`<a href="tg://user?id=${recipientUserId}">user</a> id=${recipientUserId}`
 
   const resultId = randomBytes(16).toString('hex')
 
@@ -112,9 +104,7 @@ telegram.onInlineQuery(async (update) => {
         id: resultId,
         title: `🔒 whisper to ${recipientLabel}`,
         description: 'only they can open it.',
-        content: InputMessageContent.text(`🔒 a whisper message to ${recipientMention}`, {
-          parseMode: 'html'
-        }),
+        content: InputMessageContent.text(html`🔒 a whisper message to ${recipientMention}`),
         replyMarkup: InlineKeyboard.keyboard([
           InlineKeyboard.textButton({
             text: 'show message 🔐',
